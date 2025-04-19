285076e920291ec54de04d171f172e6a67547d4d
Nathan <nhouck51@gmail.com>
2025-04-19 16:51:32 -0400

    Fix mobile scroll issues

--- src/components/RegularLayout.jsx
+++ src/components/RegularLayout.jsx
@@ -1,6 +1,6 @@
 const RegularLayout = ({children}) => {
     return (
-        <div className="px-5 pt-10 pb-2 justify-center flex max-w-[610px]">
+        <div className="px-5 pt-10 pb-20 lg:pb-2 justify-center flex max-w-[610px]">
             {children}
         </div>
     );
